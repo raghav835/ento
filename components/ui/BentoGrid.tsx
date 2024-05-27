9a7df0a9ee7d969abb04294b992ee2da297b3f63
Raghavendra GH <68004393+raghav835@users.noreply.github.com>
2024-05-27 08:32:01 +0530

    Update BentoGrid.tsx

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -67,7 +67,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "hsu@jsmastery.pro";
+    const text = "raghu3340@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
